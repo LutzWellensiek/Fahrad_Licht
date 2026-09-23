c34cc1da3e706b154960ec7371eb8a9e415b11a3
Lutz Wellensiek <72201544+LutzWellensiek@users.noreply.github.com>
2026-09-23 23:05:25 +0200

    Add files via upload
    swipie

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "signal-light-v1";
+const CACHE = "signal-light-gesture-v1";
 const FILES = ["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
